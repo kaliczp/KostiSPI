2fd57fdc2a0dce67bb85354d9cb2eeb5bd55934f
Kalicz Péter <kaliczp@gmail.com>
2025-07-18 11:53:31 +0200

    Notation about package installation

--- Kosti_SPI1.R
+++ Kosti_SPI1.R
@@ -2,6 +2,7 @@ library(readxl)
 ProjectSPI <- read_excel("ProjectSPI.xlsx")
 View(ProjectSPI)
 detach("package:SPEI", unload = TRUE)
+## install.packages("SPEI") I did not use it
 library(SPEI)
 # Package SPEI (1.8.1) loaded [try SPEINews()].
  spi3<- spi(ProjectSPI$Prcp,3)
