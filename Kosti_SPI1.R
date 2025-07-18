9816a68c48a5731ac48867669c5a452cd5e43eb5
Kalicz Péter <kaliczp@gmail.com>
2025-07-18 12:02:14 +0200

    Time-series is used as input

--- Kosti_SPI1.R
+++ Kosti_SPI1.R
@@ -5,5 +5,8 @@ detach("package:SPEI", unload = TRUE)
 ## install.packages("SPEI") I did not use it
 library(SPEI)
 # Package SPEI (1.8.1) loaded [try SPEINews()].
-spi3 <- spi(ProjectSPI$Prcp, 3)
+## Generate ts object
+Prcp <- ts(ProjectSPI$Prcp, start = 2002, frequency = 12)
+plot(Prcp)
+spi3 <- spi(Prcp, 3)
 spi3
