29a878af83c961d1f0b19c6330ade8bd42706937
Kalicz Péter <kaliczp@gmail.com>
2025-07-18 11:54:49 +0200

    Get working part of manual

--- PackageExample.R
+++ PackageExample.R
@@ -1,2 +1,5 @@
 ## load example file in spi() funciton help
 load(system.file("data/wichita.rda", package = "SPEI"))
+
+wichita <- ts(wichita[, -c(1, 2)], end = c(2011, 10), frequency = 12) # make ts
+plot(wichita) # wiew multi-variable ts
