9f33e53eef13950fc318de541483f2b207dafba4
Kalicz Péter <kaliczp@gmail.com>
2025-07-18 13:10:03 +0200

    Split database process to more lines

--- MultiSPIplot.R
+++ MultiSPIplot.R
@@ -1,7 +1,8 @@
 par(mfrow = c(5,1), mar = c(2.1,2.1,0.5,0.5), las = 1)
 for(SPIscale in c(3,6,12,18,24)) {
-    plot(spi(Prcp, scale = SPIscale)$fitted,type = "h",
-         lend = "butt",
-         xlab = "", ylab = "", xaxs = "i", lwd = 2)
+    currentspi <- spi(Prcp, scale = SPIscale, verbose = FALSE)
+    spits <- currentspi$fitted
+    plot(spits, type = "h",
+         xlab = "", ylab = "", xaxs = "i")
     legend("topright", paste0("SPI (",SPIscale," months)"), bg = "white")
 }
