a469f1d9ad4dafb60db9c291e19f7f4351d8862e
Kalicz Péter <kaliczp@gmail.com>
2025-07-18 11:53:52 +0200

    Reformatted spi command

--- Kosti_SPI1.R
+++ Kosti_SPI1.R
@@ -5,5 +5,5 @@ detach("package:SPEI", unload = TRUE)
 ## install.packages("SPEI") I did not use it
 library(SPEI)
 # Package SPEI (1.8.1) loaded [try SPEINews()].
- spi3<- spi(ProjectSPI$Prcp,3)
+spi3 <- spi(ProjectSPI$Prcp, 3)
 spi3
