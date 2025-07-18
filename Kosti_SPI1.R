c8d7ea0f8e2d559e4848c8294f9b9fb87fb1fbea
Kalicz Péter <kaliczp@gmail.com>
2025-07-18 10:43:57 +0200

    Initial version of script

---
+++ Kosti_SPI1.R
@@ -0,0 +1,8 @@
+library(readxl)
+> ProjectSPI <- read_excel("D:/publication/Drought/SPI/ProjectSPI.xlsx")
+> View(ProjectSPI)
+> detach("package:SPEI", unload = TRUE)
+> library(SPEI)
+# Package SPEI (1.8.1) loaded [try SPEINews()].
+ spi3<- spi(ProjectSPI$Prcp,3)
+spi3
