354c9c94f7ad52378937d9444c337d946663aa78
Kalicz Péter <kaliczp@gmail.com>
2025-07-18 12:42:26 +0200

    Try to modify appearance

--- MultiSPIplot.R
+++ MultiSPIplot.R
@@ -1,6 +1,7 @@
 par(mfrow = c(5,1), mar = c(2.1,2.1,0.5,0.5))
 for(SPIscale in c(3,6,12,18,24)) {
     plot(spi(Prcp, scale = SPIscale)$fitted,type = "h",
-         xlab = "", ylab = "", xaxs = "i", lwd = 3)
+         lend = "butt",
+         xlab = "", ylab = "", xaxs = "i", lwd = 2)
     legend("topright", paste0("SPI (",SPIscale," months)"))
 }
